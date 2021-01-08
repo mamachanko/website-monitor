0896057ef811023776fcc52c24a863754fc053dc
mamachanko <bfahc2cbv@relay.firefox.com>
2021-01-08 09:53:52 +0100

    Commit to Kafka stream after successfully writing to db

--- website_monitor/consume_and_write.py
+++ website_monitor/consume_and_write.py
@@ -2,9 +2,10 @@ from website_monitor import stream, database, env
 
 
 def main():
-    url_probes = stream.consume()
+    url_probes, commit = stream.consume()
     db = database.Database(env.require_env("WM_DB_CONNECTION_STRING"))
     db.store(url_probes)
+    commit()
 
 
 if __name__ == '__main__':
--- website_monitor/stream.py
+++ website_monitor/stream.py
@@ -28,21 +28,30 @@ def consume():
         ssl_cafile=require_env("WM_STREAM_SSL_CA_FILE"),
         ssl_certfile=require_env("WM_STREAM_SSL_CERT_FILE"),
         ssl_keyfile=require_env("WM_STREAM_SSL_KEY_FILE"),
-        api_version=(0, 10, 2),
+        api_version=(2,),
         auto_offset_reset="earliest",
         enable_auto_commit=False
     )
 
     # Inspired by
     # https://help.aiven.io/en/articles/489572-getting-started-with-aiven-kafka
-    url_probes = []
-    for _ in range(2):
-        for raw_url_probes in consumer.poll(timeout_ms=1000).values():
+
+    url_probes: list[UrlProbe] = []
+    poll_count = 0
+    while poll_count := poll_count + 1:
+        poll = consumer.poll(timeout_ms=1000, max_records=10)
+
+        # poll at least twice and until there are no more records
+        if poll_count > 1 and len(poll) == 0:
+            break
+
+        for raw_url_probes in poll.values():
             for url_probe_json in raw_url_probes:
                 url_probe = UrlProbe.from_json(url_probe_json.value.decode("utf-8"))
                 url_probes.append(url_probe)
 
-    consumer.commit()
-    consumer.close()
+    def commit():
+        consumer.commit()
+        consumer.close()
 
-    return url_probes
+    return url_probes, commit
