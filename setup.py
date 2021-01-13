6ab86d596f42cab5a3da3f48094b676c183bed3f
mamachanko <bfahc2cbv@relay.firefox.com>
2021-01-13 14:04:13 +0100

    Include package in installation

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="wm",
-    version="0.0.2",
+    version="0.0.3",
     py_modules=["website_monitor"],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "kafka-python==2.0.2",
         "psycopg2==2.8.6",
