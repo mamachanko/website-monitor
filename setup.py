cfa8a4514d59f2ae54c55cf8e44177d4dc8b9b06
mamachanko <bfahc2cbv@relay.firefox.com>
2021-01-21 14:32:16 +0100

    Bump version to 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wm",
-    version="0.0.5",
+    version="0.0.6",
     py_modules=["website_monitor"],
     packages=find_packages(),
     include_package_data=True,
